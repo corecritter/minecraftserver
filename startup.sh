c0e478ce49fb0a0fb4da63813ec4c2054871e91c
corecritter <bobbysavel@gmail.com>
2024-03-08 10:51:54 -0500

    cleanup and fix some line ending issues

--- startup.sh
+++ startup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-cp /var/install/eula.txt /var/data/minecraft/eula.txt
-cp /var/install/server.jar /var/data/minecraft/server.jar
+cp /var/install/eula.txt /var/minecraft/eula.txt
+cp /var/install/server.jar /var/minecraft/server.jar
 
-for filename in /var/settings/; do
-    if ! test -f /var/data/minecraft/$filename; then
+for filename in /var/settings/*; do
+    if ! test -f /var/minecraft/$filename; then
         echo "copying $filename because it does not exist"
-        cp /var/settings/$filename /var/data/minecraft/$filename
+        cp $filename file_name=$(basename "$filename")
     fi
 done
 
-cd /var/data/minecraft
+cd /var/minecraft
 java -Xmx1024M -Xms1024M -jar server.jar
