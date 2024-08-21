eb7e1cafe64abcd61e80e0c149e1b21f064c57ce
Bobby Savel <bobbysavel@gmail.com>
2024-08-21 16:33:06 -0400

    use nfs server

--- apply.ps1
+++ apply.ps1
@@ -1,9 +1,13 @@
 docker build ./ -t 192.168.50.205:32000/mc:latest
 docker push 192.168.50.205:32000/mc:latest
 
+kubectl delete namespace mc
+kubectl delete pv ftp-pv
+
+kubectl apply -f ./kube/sc-nfs.yaml
 kubectl apply -f ./kube/namespace.yaml
-kubectl apply -f ./kube/01.persistent_volume.yaml
-kubectl apply -f ./kube/02.pvc_claim.yaml
+#kubectl apply -f ./kube/01.persistent_volume.yaml
+kubectl apply -f ./kube/pvc_claim.yaml
 kubectl apply -f ./kube/deployment.yaml
 kubectl apply -f ./kube/service.yaml
 kubectl apply -f ./kube/ingress.yaml
